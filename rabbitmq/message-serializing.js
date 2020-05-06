63d3258f2cce0d25f1f7443b80953b8681cec877
Oscar Olsson <osse.olsson@gmail.com>
2020-05-06 08:42:34 +0200

    Try parse json insead of error

--- rabbitmq/message-serializing.js
+++ rabbitmq/message-serializing.js
@@ -1,7 +1,15 @@
 const R = require('ramda');
 
+function tryParseJson(json) {
+  try {
+    return JSON.parse(json);
+  } catch {
+    return json;
+  }
+}
+
 const deserialize = R.cond([
-  [ R.pathSatisfies(R.equals('application/json'), [ 'properties', 'contentType' ]), R.over(R.lensProp('content'), JSON.parse) ],
+  [ R.pathSatisfies(R.equals('application/json'), [ 'properties', 'contentType' ]), R.over(R.lensProp('content'), tryParseJson) ],
   [ R.T, R.identity ]
 ]);
 
