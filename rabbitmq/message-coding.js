66c53a5e72bc1758a14799c64c72f8fa18a3272f
Oscar Olsson <osse.olsson@gmail.com>
2020-05-06 08:42:00 +0200

    Support content_encoding prop

--- rabbitmq/message-coding.js
+++ rabbitmq/message-coding.js
@@ -5,11 +5,13 @@ const gunzip = (data) => zlib.gunzipSync(data);
 const gzip = (data) => zlib.gzipSync(data);
 
 const decode = R.cond([
+  [ R.pathSatisfies(R.equals('gzip'), [ 'properties', 'contentEncoding' ]), R.over(R.lensProp('content'), R.pipe(gunzip, R.toString)) ],
   [ R.pathSatisfies(R.equals('gzip'), [ 'properties', 'headers', 'Content-Encoding' ]), R.over(R.lensProp('content'), R.pipe(gunzip, R.toString)) ],
   [ R.T, R.over(R.lensProp('content'), R.toString) ]
 ]);
 
 const encode = R.cond([
+  [ R.pathSatisfies(R.equals('gzip'), [ 'properties', 'contentEncoding' ]), R.over(R.lensProp('content'), R.pipe(R.curryN(1, Buffer.from), gzip)) ],
   [ R.pathSatisfies(R.equals('gzip'), [ 'properties', 'headers', 'Content-Encoding' ]), R.over(R.lensProp('content'), R.pipe(R.curryN(1, Buffer.from), gzip)) ],
   [ R.T, R.over(R.lensProp('content'), R.curryN(1, Buffer.from)) ]
 ]);
